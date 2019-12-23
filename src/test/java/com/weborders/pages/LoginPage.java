3ece4d1f23d7d49914e2431c1889852357a47aab
your_github_name <your_github_email>
2019-12-23 15:18:20 -0600

    confprop added

--- src/test/java/com/weborders/pages/LoginPage.java
+++ src/test/java/com/weborders/pages/LoginPage.java
@@ -1,9 +1,16 @@
 package com.weborders.pages;
 
+import com.weborders.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
+    public  LoginPage(){
+        PageFactory.initElements(Driver.get(),this);
+        }
+
+
     @FindBy(id="#ctl00_MainContent_username")
     public WebElement username;
 
